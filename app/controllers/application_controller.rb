2f3b96f9dcc3f0093cba06a023e77747dda61c41
Sharon Watkins <swatkins876@gmail.com>
2019-12-10 16:49:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,17 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+
+    post '/teams' do
+      @name = params[:team][:name]
+      @motto = params[:team][:motto]
+      @super_heros = params[:team][:members]
+
+      erb :team
+    end
 
 end
